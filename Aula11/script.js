2d68e7f83cf9aeb85a3e4ed1b398888307e16385
Milton <mpercinio@gmail.com>
2023-08-16 15:53:35 -0300

    projeto codifica

--- Aula11/script.js
+++ Aula11/script.js
@@ -6,46 +6,40 @@ function validationForm() {
     let validationActor = document.forms["register"]["actor"].value;
     let validationIMDb = document.forms["register"]["imdb"].value;
     let validationRadio = document.forms["register"]["radio"].value;
-
+  
     if (validationCod == "") {
-        alert("O Código deve ser preenchido");
-        return false;
+      alert("O Código deve ser preenchido");
+      return false;
     }
-
     if (validationTitle == "") {
-        alert("O Titulo deve ser preenchido");
-        return false;
+      alert("O titulo deve ser preenchido");
+      return false;
     }
-
     if (validationDirector == "") {
-        alert("O Diretor deve ser preenchido");
-        return false;
+      alert("O Nome do diretor deve ser preenchido");
+      return false;
     }
-
     if (validationDate == "") {
-        alert("A data deve ser preenchida");
-        return false;
+      alert("A data deve ser preenchida");
+      return false;
     }
-
     if (validationActor == "") {
-        alert("O Ator ou Atriz principal deve ser preenchido");
-        return false;
+      alert("O Ator ou Atriz principal deve ser preenchido");
+      return false;
     }
-
     if (validationIMDb == "") {
-        alert("A Nota do IMDb deve ser preenchida");
-        return false;
+      alert("A Nota do IMDb deve ser preenchida");
+      return false;
     }
-
     if (validationRadio == "") {
-        alert("O Gênero deve ser preenchido");
-        return false;
+      alert("O Gênero deve ser preenchido");
+      return false;
     }
-
-    alert("A Validação ocorreu de forma correta")
-}
-
-function insert() {
+  
+    alert("A validação ocorreu de forma correta");
+  }
+  
+  function insert() {
     let cod = document.forms["register"]["cod"].value;
     let title = document.forms["register"]["title"].value;
     let director = document.forms["register"]["director"].value;
@@ -53,17 +47,16 @@ function insert() {
     let actor = document.forms["register"]["actor"].value;
     let IMDb = document.forms["register"]["imdb"].value;
     let radio = document.forms["register"]["radio"].value;
-
+  
     let insert = window.document.getElementById("insertRow");
-
+  
     insert.innerHTML = `
     <th scope="row">${cod}</th>
     <td>${title}</td>
     <td>${director}</td>
     <td>${date}</td>
     <td>${actor}</td>
-    <td>${IMDbcd}</td>
+    <td>${IMDb}</td>
     <td>${radio}</td>
     `;
-    
-}
+  }
